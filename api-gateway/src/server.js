0798214dbdeb082b57020cfdcad5641343b2a8ee
abhishektw456 <abhishek.singh@thoughtwin.com>
2026-03-09 20:57:12 +0530

    setting up proxy server

---
+++ api-gateway/src/server.js
@@ -0,0 +1,83 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import helmet from "helmet";
+import cors from "cors";
+import Redis from "ioredis";
+import rateLimit from "express-rate-limit";
+
+import RedisStore from "rate-limit-redis";
+import logger from "./utils/logger.js";
+import proxy from "express-http-proxy";
+import errorHandler from "./middleware/errorHandler.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const redisClient = new Redis(process.env.REDIS_URL);
+
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+const ratelimitOptions = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 50,
+  standardHeaders: true,
+  legacyHeaders: false,
+  handler: (req, res) => {
+    logger.warn(`sensitive endpoints`);
+    res.status(429).json({ success: false, message: "too many requests" });
+  },
+  store: new RedisStore({
+    sendCommand: (...args) => redisClient.call(...args),
+  }),
+});
+
+app.use(ratelimitOptions);
+
+app.use((req, res, next) => {
+  logger.info(`received ${req.method} req to ${req.url}`);
+  logger.info(`request body, ${req.body}`);
+  next();
+});
+
+const proxyOptions = {
+  proxyReqPathResolver: (req) => {
+    return req.originalUrl.replace(/^\/v1/, "/api");
+  },
+  proxyErrorHandler: (err, res, next) => {
+    logger.error(`Proxy error: ${err.message}`);
+    res.status(500).json({
+      message: `Internal server error`,
+      error: err.message,
+    });
+  },
+};
+
+app.use(
+  "/v1/auth",
+  proxy(process.env.IDENTITY_SERVICE_URL, {
+    ...proxyOptions,
+    proxyReqOptDecorator: (proxyReqOpts, srcReq) => {
+      proxyReqOpts.headers["Content-Type"] = "application/json";
+      return proxyReqOpts;
+    },
+    userResDecorator: (proxyRes, proxyResData, useReq, userRes) => {
+      logger.info(
+        `Response received from identity service: ${proxyRes.statusCode}`,
+      );
+      return proxyResData;
+    },
+  }),
+);
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  logger.info(`api gateway is running on ${PORT}`);
+  logger.info(
+    `identity service running on ${process.env.IDENTITY_SERVICE_URL}`,
+  );
+  logger.info(`Redis url ${process.env.REDIS_URL}`);
+});
---
+++ readme.md
@@ -0,0 +1,51 @@
+=>
+update this in ur package.json file under script object
+"start": "node src/server.js",
+"dev": "nodemon src/server.js"
+
+=>
+add this in root object in package.json
+"type": "module",
+
+=>
+run this to install nodemon
+npm i nodemon --save-dev
+
+=>
+to install docker in ubuntu run these commands:-
+sudo apt install ca-certificates curl gnupg
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | \
+ sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+echo \
+ "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] \
+ https://download.docker.com/linux/ubuntu \
+ $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+ sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+=>
+to check if docker installed or not
+docker --version
+
+=>
+to check if docker successfully working
+sudo docker run hello-world
+
+=>
+if facing permission issue on docker commands withouts sudo run these commands:-
+groups
+sudo usermod -aG docker tw
+newgrp docker
+
+=>
+to see all running services => docker ps
+
+=>
+to start service => docker run -d --name redis-server -p 6379:6379 redis
+
+=> to run api hit this
+http://localhost:3000/v1/auth/register
